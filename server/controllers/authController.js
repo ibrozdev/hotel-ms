39125f3d3c7256945b5f140d83cc16efefb47f19
ibrozdev <ibraahim3523@gmail.com>
2026-01-07 16:59:26 +0300

    added  user login with token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,3 +26,35 @@ export const register = async (req,res)=>{
 }
 
 
+
+export const login = async  (req,res)=>{
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "User does not exist" });
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch) return res.status(400).json({ message: "invalid password" });
+
+        const token = JWT.sign(
+          { id: user._id, role: user.role },
+          process.env.JWT_SECRET,
+          {expiresIn: '1d'}
+        );
+
+        res.status(200).json({
+            success: true,
+            message: 'User Logged in successfully',
+            user:{
+                id: user._id,
+                name: user.name,
+                userEmail: user.email,
+                role: user.role,
+                token
+            }
+        })
+
+    } catch (err) {
+        res.status(500).json({ success: false, Error: err.message });       
+    }
+}
